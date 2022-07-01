b321ff9424c50eeb48bad4b1d09700ed2de812e2
Bikash Mainali <b.mainali02@gmail.com>
2022-06-30 22:53:20 -0500

    adsf

--- src/main/java/com/example/docker/DockerApplication.java
+++ src/main/java/com/example/docker/DockerApplication.java
@@ -2,17 +2,25 @@ package com.example.docker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class DockerApplication {
+public class DockerApplication extends SpringBootServletInitializer {
     @GetMapping("/message")
     public String getMessage(){
         System.out.println("....dddd.");
         return "Welcome to docker";
     }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        return application.sources(DockerApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(DockerApplication.class, args);
     }
